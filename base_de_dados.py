a9027c466beca50b46aa89c26ae98d5bb6fdbc82
Wilson Calixto Cordovil junior <wilsoncalisto@users.noreply.github.com>
2018-04-05 14:57:50 -0400

    Add files via upload

---
+++ base_de_dados.py
@@ -0,0 +1,260 @@
+topicos =['economia internacional','economia ibovespa','economia comercio internacional',
+  	'combate a corrupcao',  'greves','politica eleicoes 2018','entretenimento automotivo',
+  	 'entretenimento motomotivo','games','enem','culinaria','cinema','mundo','ciencia e saude',
+  	 'bem estar','turismo', 'saude publica', 'segurança publica','tecnologia','midia e marketing',
+  	 'religião','tragédias','meio ambiente','acidentes','pop e art','futebol dentro de campo',
+  	 'futebol bastidores','educacao','guerras civis']
+avaliacoes = {
+
+  'economia internacional':
+      {'Wilson': 4.5,
+       'Matheus': 3.5,
+       'Catanhede': 0.5,
+       'Maria': 0.0,
+       
+       'Ramon': 0.0},
+
+
+  'economia ibovespa':
+      {'Wilson': 3.5,
+       'Matheus': 4.0,
+       'Catanhede': 0.5,
+       'Jocemar': 3.0,
+       'Carla': 4.5,
+       'Jose': 4.5,
+       'Ramon': 0.0},
+	
+  'economia comercio internacional':
+      {'Matheus': 2.0,
+       'Carla': 4.5,
+       'Ana': 2.5,
+       'Ramon': 0.0},
+
+
+	'combate a corrupcao':
+      {'Wilson': 4.0,
+      'Alice': 4.0,
+       'Matheus': 4.5,
+       'Carla': 4.5,
+       'Carlos Adriano': 3.5,
+       'Raimunda': 3.5,
+       'Michele': 3.0,
+       'Jose': 5.0,
+       'Jocemar': 3.0,
+       'Ramon': 4.0},
+
+  'greves':
+      {'Wilson': 0.5,
+      'Alice': 5.0,
+       'Matheus': 2.5,
+       'Catanhede': 5.0,
+       'Carla': 1.5,
+       'Michele': 1.0,
+       'Carlos Adriano': 2.5,
+       'Raimunda': 3.5,
+       'Jose': 1.5,
+       'Maria': 2.0,
+	   'Ramon': 4.0},
+
+  'politica eleicoes 2018':
+      {'Wilson': 4.0,
+       'Matheus': 3.5,
+       'Carla': 4.5,
+       'Carlos Adriano': 3.5,
+       'Alice': 1.0,
+
+       'Catanhede': 5.0,
+       'Jose': 4.5,
+       'Ramon': 1.0},
+
+
+  'entretenimento automotivo':
+      {'Wilson': 4.5,
+       'Matheus': 1.5,
+       'Carla': 3.5,
+       'Jocemar': 3.0,
+       'Luiz': 4.5,
+        'Carlos Adriano': 3.5,
+      'Catanhede': 4.5},
+
+
+  'entretenimento motomotivo':
+      {'Wilson': 0.5,
+      'Jocemar': 3.0,
+       'Matheus': 4.0},
+
+	'games':
+      {'Wilson': 4.0,
+      'Catanhede': 2.5,      
+       'Matheus': 4.5,      
+       'Carlos Adriano': 2.5,
+       'Jocemar': 4.0,
+       'Luiz': 4.5
+
+       },
+
+'enem':
+      {'Matheus': 3.5,
+       'Marcos': 3.5,
+       'Alice': 3.0,
+       'Maria': 4.5},
+  
+
+
+'culinaria':
+      {
+      'Carlos Adriano': 4.5,
+      'Raimunda': 4.5,
+      'Jocemar': 2.0,
+      'Michele': 3.0,
+      'Maria': 4.5,
+      'Ramon': 4.0},
+
+
+  'cinema':
+      {
+       'Carlos Adriano': 4.5,
+	   'Catanhede': 4.5,
+       'Carla': 4.5,
+       'Jocemar': 3.5,
+       'Alice': 3.0,
+       'Michele': 2.0,
+       'Maria': 4.5,
+       'Ramon': 4.0},
+
+
+  'mundo':
+      {'Wilson': 4.5,
+       'Matheus': 3.5,
+       'Carla': 4.0,
+       'Alice': 1.0,
+       'Jocemar': 3.0,
+       'Catanhede': 3.5,
+       'Jose': 4.5},
+
+  'ciencia e saude':
+      {'Jose': 3.5,
+      'Jocemar': 3.0,
+      'Luiz': 3.5},
+
+	'bem estar':
+      {'Carla': 2.5,
+      'Michele': 4.0,
+       'Alice': 4.0,
+       'Raimunda': 4.5,
+       'Jose': 4.5,
+       'Maria': 4.5,
+       'Ramon': 4.0},
+	
+
+
+
+  'futebol dentro de campo':
+      {'Wilson': 4.5,
+       'Matheus': 4.5,
+       'Jocemar': 5.0,
+       'Catanhede': 4.5,
+       'Carla': 5.0,
+       'Carlos Adriano': 5.0,
+       'Jose': 5.0},
+
+  'futebol bastidores':
+      {'Wilson': 3.5,
+      'Jocemar': 4.5,
+      'Catanhede': 4.5,
+       'Matheus': 0.5,
+       'Carla': 4.5,
+       'Carlos Adriano': 5.0,      
+       'Jose': 3.5},
+
+
+
+  'midia e marketing':
+      {'Ramon': 4.0},
+
+
+  'segurança publica':
+      {'Wilson': 4.5,
+       'Matheus': 4.0,
+       'Catanhede': 4.5,
+       'Carla': 4.5,
+       'Raimunda': 1.5,
+       'Jose': 4.5},
+       
+
+  'saude publica':
+      {'Carla': 1.5,
+      'Alice': 3.0,
+      'Michele': 2.0,
+       
+       'Raimunda': 1.5,
+       'Maria': 3.5},
+
+  'tecnologia':
+      {'Wilson': 4.5,
+	   'Carlos Adriano': 1.0,
+       'Catanhede': 3.5,
+       'Jocemar': 3.0,       
+       'Jose': 4.5},
+
+  'turismo':
+      {'Carlos Adriano': 4.0,
+      'Maria': 3.5,
+      'Alice': 5.0,
+      'Jocemar': 5.0,
+      'Michele': 4.0,
+      'Ramon': 4.0},
+
+  'religião':
+      {'Catanhede': 4.5,
+		'Carla': 4.5,
+		'Michele': 4.0,
+		'Alice': 4.0,
+		'Jose': 4.5,
+		'Maria': 3.0},
+
+  'tragédias':
+      {'Catanhede': 3.5,
+      'Michele': 5.0,
+      'Alice': 4.0,
+       'Carlos Adriano': 4.5,
+       'Jocemar': 3.0,
+       'Raimunda': 5.0,
+       'Maria': 3.0,
+       'Ramon': 4.0},
+
+
+'meio ambiente':
+      {'Catanhede': 4.5,
+      'Ramon': 4.0},
+
+  'acidentes':
+      {'Carla': 2.5,
+       'Catanhede': 4.5,
+       'Carlos Adriano': 4.0,
+       'Jocemar': 3.5,
+       'Alice': 3.5,
+       'Michele': 4.0,
+       'Raimunda': 5.0,
+       'Jose': 2.5,
+       'Maria': 2.5},
+
+  'pop e art':
+      {'Maria': 3.5,
+      'Alice': 3.0,
+      'Ramon': 4.0},       
+
+	
+
+  'educacao':
+      {'Catanhede': 4.5,
+      'Alice': 4.0,
+      'Maria': 3.5,
+      'Ramon': 3.0},              
+
+  'guerras civis':
+      {
+      'Catanhede': 4.5,
+	  'Jose': 2.5}
+
+  }
